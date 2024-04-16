da552e6151cb6b2562ba72927d00a687f16ff1fd
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-04-16 15:04:26 +0300

    feat

--- lib/const/enum.dart
+++ lib/const/enum.dart
@@ -1,13 +1,13 @@
 enum PaycellMethodType {
-  salesCash('Nakit Satış', 1),
-  salesCashVoid('Nakit Satış İptali', -1),
-  salesCard('Kartlı Satış', 2),
-  salesCardVoid('Kartlı Satış İptali', -2),
-  refund('İade', 3),
-  refundVoid('İadenin iptali', -3);
+  salesCash('Nakit Satış', '1'),
+  salesCashVoid('Nakit Satış İptali', '-1'),
+  salesCard('Kartlı Satış', '2'),
+  salesCardVoid('Kartlı Satış İptali', '-2'),
+  refund('İade', '3'),
+  refundVoid('İadenin iptali', '-3');
 
   final String title;
-  final int type;
+  final String type;
 
   const PaycellMethodType(this.title, this.type);
 }
